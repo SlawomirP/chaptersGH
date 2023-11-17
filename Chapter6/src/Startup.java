f6447d703988e5db8ab90c7699802c04f3bc97ae
Sławomir P <slawekpettke@gmail.com>
2023-11-17 18:43:10 +0100

    17.11.2023

--- Chapter6/src/Startup.java
+++ Chapter6/src/Startup.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Startup {
+
+    private ArrayList<String> locationCells;
+
+    public void setLocationCells(ArrayList<String> locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public String checkYourself(String userInput){
+        String result = "miss";                         // standardowo metoda zwróci missa
+
+        int index = locationCells.indexOf(userInput); // pod index przypiszemy miejsce pod ktorym
+                                            // znajduje sie argument uzytkownika - jezeli go nie bedzie to zwroci -1
+
+        if (index >= 0){                        // jezeli cos sie znalazlo w liscie to index tego bedzie na pewno >= 0
+            locationCells.remove(index);                 // w tym momencie usuwamy ten el z listy
+        }
+
+        if (locationCells.isEmpty()){           // sprawdzenie czy lista jest juz pusta
+            result = "kill";                    // jezeli tak to znaczy zestrzelony
+        } else {
+            result = "hit";                     // jezeli nie tzn ze tylko trafiony bo cos jeszcze zostalo na liscie
+        }
+        return result;
+    }
 }
