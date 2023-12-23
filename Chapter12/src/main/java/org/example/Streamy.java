05430f9150c5695753de4a2c936a1cec1370f4de
Sławomir P <slawekpettke@gmail.com>
2023-12-23 13:37:39 +0100

    23/12/2023

--- Chapter12/src/main/java/org/example/Streamy.java
+++ Chapter12/src/main/java/org/example/Streamy.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Streamy {
+    public static void main(String[] args) {
+        List<String> strings = List.of("I", "am", "a", "list", "of", "Strings");
+
+        // przyklad z posortowaniem i potem ograniczeniem ilosci elementow
+
+        List<String> result = strings.stream()
+//                .sorted()
+                .skip(2)
+                .limit(4)
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+
+
+    }
 }
