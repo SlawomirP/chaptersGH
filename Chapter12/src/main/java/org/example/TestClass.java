2351dbc6432a08c2d94b6a6d1046592e8a01ac32
Sławomir P <slawekpettke@gmail.com>
2023-12-26 13:45:37 +0100

    26/12/2023

--- Chapter12/src/main/java/org/example/TestClass.java
+++ Chapter12/src/main/java/org/example/TestClass.java
@@ -1,19 +1,41 @@
 package org.example;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TestClass {
     public static void main(String[] args) {
-        Songs songs = new Songs();
+        Songs mockSongs = new Songs();
 
-        List<Song> louList = songs.getSongs();
+        List<Song> songs = mockSongs.getSongs();
 
         // z listy wybierzemy tylko piosenki z typu rock
-        List<Song> rockSongs = louList.stream()
-                .filter(s -> s.getGenre().equals("Rock"))
+        List<Song> rockSongs = songs.stream()
+                .filter(s -> s.getGenre().toLowerCase().contains("rock"))
                 .collect(Collectors.toList());
 
-        System.out.println(rockSongs);
+//        System.out.println(rockSongs);
+
+        // z listy wybrane zostana tylko piosenki Beatlesów
+        List<Song> firstH = songs.stream()
+                .filter(s -> s.getYear()>1995)
+                .collect(Collectors.toList());
+
+//        System.out.println(firstH);
+
+        // zamiana pierwotnych elementow w liscie na inne (zawarte w obiekcie)
+        // plus usuniecie duplikatów z listy
+        List<String> onlyGenre = songs.stream()
+                .map(s -> s.getGenre())
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
+
+        System.out.println(onlyGenre);
+
+        Set<String> genres = songs.stream()
+                .map(song -> song.getGenre())
+                .collect(Collectors.toUnmodifiableSet());
+
     }
 }
