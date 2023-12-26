721afb931e16ee9b2f4773aabc815a5a1cc69801
Sławomir P <slawekpettke@gmail.com>
2023-12-26 17:08:02 +0100

    26/12/2023

--- Chapter12/src/main/java/org/example/TestClass.java
+++ Chapter12/src/main/java/org/example/TestClass.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,6 +37,40 @@ public class TestClass {
         Set<String> genres = songs.stream()
                 .map(song -> song.getGenre())
                 .collect(Collectors.toUnmodifiableSet());
+
+        boolean resultAnyMatch = songs.stream()
+                .anyMatch(s -> s.getGenre().equals("R&B"));
+        System.out.println(resultAnyMatch);
+
+        boolean resultAllMatch = songs.stream()
+                .allMatch(s -> s.getGenre().equals("R&B"));
+        System.out.println(resultAllMatch);
+
+        boolean resultNoneMatch = songs.stream()
+                .noneMatch(s -> s.getGenre().equals("R&B"));
+        System.out.println(resultNoneMatch);
+
+        Optional<Song> resultFindFirst = songs.stream()
+                .filter(s -> s.getYear() == 1995)
+                .findFirst();
+        System.out.println(resultFindFirst);
+
+        Optional<Song> resultFindAny = songs.stream()
+                .filter(s -> s.getTitle().equals("Hurt"))
+                .findAny();
+        System.out.println(resultFindAny);
+
+        Optional<Integer> resultMaxYear = songs.stream()
+                        .map(Song::getYear)
+                                .max(Integer::compareTo);
+
+        System.out.println(resultMaxYear);
+
+        long result = songs.stream()
+                .map(Song :: getArtist)
+                .distinct()
+                .count();
+        System.out.println(result);
 
     }
 }
