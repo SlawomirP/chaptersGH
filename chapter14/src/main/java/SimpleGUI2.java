518a6b9e10b37b0664a78a78310f74f6a45ec53c
Sławomir P <slawekpettke@gmail.com>
2024-01-06 20:11:05 +0100

    06/01/2024

--- chapter14/src/main/java/SimpleGUI2.java
+++ chapter14/src/main/java/SimpleGUI2.java
@@ -1,2 +1,31 @@
-public class SimpleGUI2 {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGUI2 implements ActionListener {
+
+    private JButton button;
+
+    public static void main(String[] args) {
+        SimpleGUI2 gui = new SimpleGUI2();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+
+        button.addActionListener(this);
+
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("I've been clicked");
+    }
 }
