13bbafbd13ef9ccc128e70f32d84e28f33fc6816
SlawP <slawekpettke@gmail.com>
2024-01-13 17:54:54 +0100

    13/01/2024

--- chapter14/src/main/java/MyDrawPanel.java
+++ chapter14/src/main/java/MyDrawPanel.java
@@ -4,15 +4,15 @@ import java.util.Random;
 
 public class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
-        g.fillRect(0,0, this.getWidth(), this.getHeight());
+        g.fillRect(0,0, this.getWidth(), this.getHeight()); // wypelnienie na czarno calego okna
 
         Random random = new Random();
         int red = random.nextInt(256);
         int green = random.nextInt(256);
         int blue = random.nextInt(256);
 
-        Color randomColor = new Color (red, green, blue);
-        g.setColor(randomColor);
-        g.fillOval(50,50,100,100);
+        Color randomColor = new Color (red, green, blue); // utworzenie obiektu koloru
+        g.setColor(randomColor); // ustawienie go jako koloru obiektu g
+        g.fillOval(50,50,100,100); // narysowanie kola
     }
 }
--- chapter14/src/main/java/SimpleGui3.java
+++ chapter14/src/main/java/SimpleGui3.java
@@ -3,30 +3,33 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SimpleGui3 implements ActionListener {
+public class SimpleGui3 implements ActionListener { // implementacja listenera do buttona
 
     private JFrame frame;
 
     public void go () {
 
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame = new JFrame(); // przypisanie obiektu do referenji
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // zamkniecie ramki po zamkn okna
 
-        JButton button = new JButton("Here you can change color");
-        button.addActionListener(this);
+        JButton button = new JButton("Here you can change color"); // utwprzenie butona z nazwa
+        button.addActionListener(this); // rejestracja w buttonie listenera
 
 
-        MyDrawPanel drawPanel = new MyDrawPanel();
+        MyDrawPanel drawPanel = new MyDrawPanel(); // stworzenie obiektu rysujacego
 
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
-        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
-        frame.setSize(300,300);
-        frame.setVisible(true);
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);// wstawienie buttona na dole
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);//wstawienie rysowania na srodku
+        frame.setSize(300,300);//ustawienie wymiaru frame'a
+        frame.setVisible(true);// ustawienie widocznosci
 
     }
     @Override
     public void actionPerformed (ActionEvent e){
         frame.repaint();
-    }
+    } //nadpisana metoda ktora jest wywolana
+                //w przypadku pojawienia sie eventu na buttonie - repaint powoduje odswiezenie komponentow
+                //w ramce
 
 }
