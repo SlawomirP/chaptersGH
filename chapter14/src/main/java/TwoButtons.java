653db9aba1f4db647f6c388812d827bb10dbcbe7
SlawP <slawekpettke@gmail.com>
2024-01-14 17:47:39 +0100

    14/01/2024

--- chapter14/src/main/java/TwoButtons.java
+++ chapter14/src/main/java/TwoButtons.java
@@ -5,8 +5,8 @@ import java.awt.event.ActionListener;
 
 public class TwoButtons { // nie trzeba implementowac ActionListenera
 
-    private JFrame frame;
-    private JLabel label;
+    private JFrame frame; //ramka
+    private JLabel label; // etykietka do zmiany teksu
 
     public static void main(String[] args) {
         TwoButtons gui = new TwoButtons();
@@ -18,37 +18,37 @@ public class TwoButtons { // nie trzeba implementowac ActionListenera
         frame = new JFrame();
         label = new JLabel();
 
-        JButton labelButton = new JButton("here change label");
-        labelButton.addActionListener(new LabelListener());
+        JButton labelButton = new JButton("here change label"); // stworzenie przucisku to zmiany taxtu w etykiecie
+        labelButton.addActionListener(event -> label.setText("Clicked")); // przypisanie mu listenera, bedzie to obiekt klasy
+                                                            // wewnetrznej ktory reaguje na klikniecie
 
 
-        JButton colorButton = new JButton("Change circle color");
-        colorButton.addActionListener(new ColorListener());
+        JButton colorButton = new JButton("Change circle color"); // tu kolejny przycisk do narysowania kola
+        colorButton.addActionListener(event -> frame.repaint()); // przypisanie mu listenera z drugiej klasy wewnetrznej
 
-        label = new JLabel("Here is label");
-        MyDrawPanel myDrawPanel = new MyDrawPanel();
+        label = new JLabel("Here is label"); // obiekt etykietki z napisem
+        MyDrawPanel myDrawPanel = new MyDrawPanel(); // obiekt kroty rysuje
 
-        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton); // rozmieszczenie komponentow w frame
         frame.getContentPane().add(BorderLayout.CENTER, myDrawPanel);
         frame.getContentPane().add(BorderLayout.EAST, labelButton);
         frame.getContentPane().add(BorderLayout.WEST, label);
 
-        frame.setSize(500,400);
+        frame.setSize(500,400); // ustawienia frame'a
         frame.setVisible(true);
     }
 
-    class LabelListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            label.setText("Clicked");
-        }
-    }
-
-    class ColorListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            frame.repaint();
-        }
-    }
-
+//    class LabelListener implements ActionListener { // klasa wewnetrza reagujaca na klik buttona od labela
+//        @Override
+//        public void actionPerformed(ActionEvent e) { // i metoda zmieniajaca napis
+//            label.setText("Clicked");
+//        }
+//    }
+//
+//    class ColorListener implements ActionListener { // klasa wewnetrzna od rysowania kolka
+//        @Override
+//        public void actionPerformed(ActionEvent e) { // i metoda powodujaca ponownie wysiwetlenie w ramce
+//            frame.repaint();                        // uaktywniajaca wszystkie metody paintComponents()
+//        }
+//    }
 }
