41a569362a808a84fdeabdb795370583167a1b8b
SlawP <slawekpettke@gmail.com>
2024-01-18 19:39:28 +0100

    18/01/2024

--- chapter14/src/main/java/Test.java
+++ chapter14/src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        SimpleAnimation test = new SimpleAnimation();
+        InnerButton test = new InnerButton();
 
         test.go();
     }
