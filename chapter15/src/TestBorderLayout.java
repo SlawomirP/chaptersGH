e0dcbd1c7fe3adab81e5914d34b0419a07dc85ce
SlawP <slawekpettke@gmail.com>
2024-01-20 16:13:20 +0100

    20/01/2024

--- chapter15/src/TestBorderLayout.java
+++ chapter15/src/TestBorderLayout.java
@@ -4,11 +4,14 @@ import java.awt.*;
 public class TestBorderLayout {
     public void go() {
 
-        JFrame frame = new JFrame(); // stworzenie ramki
-        JButton przycisk = new JButton("click here!"); // stworzenie buttona
+        JFrame frame = new JFrame();
 
-        frame.getContentPane().add(BorderLayout.EAST, przycisk); // umieszczenie przycisku we wschodnim regionie
-        frame.setSize(200,200); // ustawienia dla ramki
+        frame.getContentPane().add(BorderLayout.NORTH, new JButton("North"));
+        frame.getContentPane().add(BorderLayout.SOUTH, new JButton("South"));
+        frame.getContentPane().add(BorderLayout.EAST, new JButton("East"));
+        frame.getContentPane().add(BorderLayout.WEST, new JButton("West"));
+        frame.getContentPane().add(BorderLayout.CENTER, new JButton("Center"));
+        frame.setSize(200,200);
         frame.setVisible(true);
 
     }
