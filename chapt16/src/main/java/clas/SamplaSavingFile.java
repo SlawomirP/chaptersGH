41de63cb45e037d4c5dad6cc78fe31265ca647fa
SlawP <slawekpettke@gmail.com>
2024-02-10 17:03:17 +0100

    10.02.2024

---
+++ chapt16/src/main/java/clas/SamplaSavingFile.java
@@ -0,0 +1,21 @@
+package clas;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SamplaSavingFile {
+    public static void main(String[] args) {
+
+        try{
+
+            FileWriter obiektZapisujacy = new FileWriter("test.txt");
+
+            obiektZapisujacy.write("This is sample test for saving.");
+
+            obiektZapisujacy.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
