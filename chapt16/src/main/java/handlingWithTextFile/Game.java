688dec10d1be64b3f9dd3ce354ffc28a96ea1a95
SlawP <slawekpettke@gmail.com>
2024-02-21 18:22:54 +0100

    21.02.2024 uzupelnic metody

---
+++ chapt16/src/main/java/handlingWithTextFile/Game.java
@@ -0,0 +1,28 @@
+package handlingWithTextFile;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Game {
+    private ArrayList<Card> cardArray;
+    private int currentCardIndex;
+    private Card currentCard;
+    private JTextArea polePrezentacji;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isPresentingAnswer;
+
+    public void go(){}
+
+    public void nextCard(){
+
+    }
+
+    private void open(){}
+
+    private void loadFile(){}
+
+    private void createCard(){}
+
+    private void showNextCard(){}
+}
